0a17c2af8860c05c82abcd6a132bb3fd5e97bd3e
priyansh7124 <priyanshagrawal806@gmail.com>
2025-07-06 22:54:20 +0530

    Completed User Registration and User Login

---
+++ README.md
@@ -0,0 +1,10 @@
+server----------
+express is used to create backend 
+cors is used connect backend with frontend
+dotenv is used when we want to use environment variables
+jsonwebtoken is used to create user authentication and store password
+bcrypt is used to encrypt the user's password to ensure that even the server can't read the actual password
+mongoose is used to connect project with mongoDB database.
+
+
+mongoDb password : Priyansh806
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Initialize the express application
 const app = express();
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Server is running</h1>");
 });
 
+app.use("/api/user", userRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
