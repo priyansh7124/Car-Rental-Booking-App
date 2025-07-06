d356c188bd2d138917e62a34e1e12862083f1bf7
priyansh7124 <priyanshagrawal806@gmail.com>
2025-07-06 17:22:21 +0530

    Initialised User Authentication

---
+++ Server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+// Initialize the express application
+const app = express();
+
+// Connect to the database!!
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
